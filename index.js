a9558167d9384236dd608500973e931ba94ad43d
Neiroin <plummy121@gmail.com>
2018-09-13 12:17:42 +0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var name=process.argv[2];
-var iter=3;
-while (process.argv[iter])
-{
-    name+=', '+process.argv[iter];
-    iter++;
-}
-console.log(`Hi ${name}!`);
+var fs = require("fs");
+var dirname=process.argv[2];
+var summarytext="var fs = require('fs'); var path=process.argv[1]; fs.readdir(path, function(err, items) { console.log(items); for(var i=0; i<items.length; i++) { console.log(items[i]); } });"
+
+fs.writeFile(`${dirname}/summary.js`, summarytext, function(error){
+ 
+                if(error) throw error; // если возникла ошибка
+
+});
