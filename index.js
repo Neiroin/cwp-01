101755b21e09f6fd0bd3443d59f5539b9574ec07
Neiroin <plummy121@gmail.com>
2018-09-26 22:14:46 +0300

    commit ye

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var fs = require("fs");
-var dirname=process.argv[2];
-var summarytext="var fs = require('fs'); var path=process.argv[1]; fs.readdir(path, function(err, items) { console.log(items); for(var i=0; i<items.length; i++) { console.log(items[i]); } });"
+const path = require('path');
+const fs = require('fs');
 
-fs.writeFile(`${dirname}/summary.js`, summarytext, function(error){
- 
-                if(error) throw error; // если возникла ошибка
+const DIR_PATH = process.argv[2];
+const EXTNAME_TXT = '.txt';
 
-});
+let copyright;
+let summaryScript_code =
+    'const fs = require(\'fs\');\n' +
+    'const path = require(\'path\');\n' +
+    '\n' +
+    '(function getFiles(baseDir) {\n' +
+    '    fs.readdir(baseDir, function (err, files){\n' +
+    '        for (let i in files) {\n' +
+    '            let currentDir = baseDir + path.sep + files[i];\n' +
+    '            fs.stat(currentDir, (err, stats) => {\n' +
+    '                    if (stats.isDirectory()) {\n' +
+    '                        getFiles(currentDir);\n' +
+    '                    } else {\n' +
+    '                        console.log(path.relative(__dirname, currentDir));\n' +
+    '                    }\n' +
+    '                }\n' +
+    '            );\n' +
+    '        }\n' +
+    '    });\n' +
+    '})(__dirname, null);';
